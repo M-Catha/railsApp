e0eaa87dc39edb97241d44aa637114e3ea19b69f
M-Catha <mattcatha@gmail.com>
2017-04-25 22:43:56 +0000

    Correct error in e-mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.deliver_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   host = 'matts-rails-tutorial.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
